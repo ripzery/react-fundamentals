e5053eaac987f5fffeb24138e5549106b6254e3c
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-08-30 16:58:27 +0700

    init branch

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
-    render(){
-        return (
-            <div>
-                <h1>Hello World</h1>
-                <b>Bold</b>
-            </div>
-        );
+    render() {
+        let txt = this.props.txt
+        return <h1>{txt}</h1>
     }
+}
+
+App.propTypes = {
+    txt: React.PropTypes.string,
+    cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+    txt: 'this is the default txt'
 }
 
 export default App
--- main.js
+++ main.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App txt="Manchester United 2 - 0 Hull City" cat={1} />, document.getElementById('app'))
+ReactDOM.render(
+    <App cat={5} txt="this is the props value"/>,
+    document.getElementById('app')
+)
