564fcf47ed05850a13b582afba9088cd45342895
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-08-30 16:42:48 +0700

    Init react-starter

---
+++ main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App txt="Manchester United 2 - 0 Hull City" cat={1} />, document.getElementById('app'))
