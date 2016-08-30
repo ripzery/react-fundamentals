7e705d42eb4de80775608146f4cb29ba2dcb543d
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-08-30 19:00:16 +0700

    Init branch

--- App.js
+++ App.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
-  render(){
-      return <h1>Hello World</h1>
-  }
+    constructor() {
+        super();
+        this.state = {val: 0};
+        this.update = this.update.bind(this)
+    }
+
+    update() {
+        this.setState({
+            val: this.state.val + 1
+        })
+    }
+
+    componentWillMount() {
+        this.setState({m: 2})
+    }
+
+    render() {
+        console.log("rendering!");
+        return (
+            <button onClick={this.update}>
+                {this.state.val * this.state.m}
+            </button>
+        )
+    }
+
+    componentDidMount() {
+        this.inc = setInterval(this.update, 500)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.inc)
+    }
 }
 
 export default App
