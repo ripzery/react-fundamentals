cb76ce2ee57c2fe33c21e53f2eb0019a9104855f
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-08-30 16:46:12 +0700

    Add hello world

--- App.js
+++ App.js
@@ -1,29 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
-  
+  render(){
+      return <h1>Hello World</h1>
+  }
 }
-
-// const Slider = (props) => {
-//     return (
-//         <input type="range" min="0" max="255" onChange={props.autobind}/>
-//     )
-// };
-
-class Slider extends React.Component {
-    render() {
-        return (
-            <div>
-                <input type="range" ref="inp" min="0" max="255" onChange={this.props.autobind}/>
-            </div>
-        )
-    }
-}
-
-App.propTypes = {
-    txt: React.PropTypes.string,
-    cat: React.PropTypes.number.isRequired
-}
-// const App = () => <h1> Manchester United will win tonight!</h1>
 
 export default App
