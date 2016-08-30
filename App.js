a74b326863183b484022f779ca40440027b66003
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-08-30 18:56:50 +0700

    Init branch

--- App.js
+++ App.js
@@ -1,9 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
-  render(){
-      return <h1>Hello World</h1>
-  }
+    constructor() {
+        super();
+        this.state = {val: 0};
+        this.update = this.update.bind(this);
+    }
+
+    update() {
+        this.setState({
+            val: this.state.val + 1
+        })
+    }
+
+    componentWillMount() {
+        console.log("mounting")
+    }
+
+    componentDidMount() {
+        console.log("mounted")
+    }
+
+    componentWillUnmount() {
+        console.log("bye!")
+    }
+
+    render() {
+        console.log("rendering!")
+        return <button onClick={this.update}>{this.state.val}</button>
+    }
 }
 
-export default App
+class Wrapper extends React.Component {
+    constructor() {
+        super();
+    }
+
+    mount() {
+        ReactDOM.render(<App />, document.getElementById("a"))
+    }
+
+    unmount() {
+        ReactDOM.unmountComponentAtNode(<App />, document.getElementById("a"))
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.mount.bind(this)}>Mount</button>
+                <button onClick={this.mount.bind(this)}>Unmount</button>
+                <div id="a"></div>
+            </div>
+        )
+    }
+}
+
+export default Wrapper
