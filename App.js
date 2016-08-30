d0f65d70476b526cb8a271396adf5703b6dcec84
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-08-30 16:55:19 +0700

    init branch

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// class App extends React.Component {
-//     render(){
-//         return <h1>Hello World</h1>
-//     }
-// }
-
-const App = () => <h1>Hello World</h1>
+class App extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>Hello World</h1>
+                <b>Bold</b>
+            </div>
+        );
+    }
+}
 
 export default App
