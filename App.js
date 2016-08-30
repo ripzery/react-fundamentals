5a14b8b9695284d439eeab88ba48818a3dbde522
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-08-30 17:01:57 +0700

    init branch

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
-    render(){
+    constructor() {
+        super();
+        this.state = {
+            txt: 'this is the state txt',
+            cat: 0
+        }
+    }
+
+    update(e) {
+        this.setState({
+            txt: e.target.value
+        })
+    }
+
+    render() {
         return (
             <div>
-                <h1>Hello World</h1>
-                <b>Bold</b>
+                <input type="text" onChange={this.update.bind(this)}/>
+                <h1>{this.state.txt}</h1>
             </div>
-        );
+        )
     }
 }
 
