bdb0988d35124d6f7cd1889303fff57e268e6760
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-08-30 18:45:48 +0700

    Init branch

--- App.js
+++ App.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
   render(){
-      return <h1>Hello World</h1>
+      return <Button>I <Heart /> React</Button>
   }
 }
 
+class Button extends React.Component {
+    render(){
+        return <button>{this.props.children}</button>
+    }
+}
+
+const Heart = () => <span className="glyphicon glyphicon-heart"></span>
 export default App
