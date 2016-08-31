da510abb4a85ef6e1b98a6102e8c267b0192e446
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-08-31 12:43:17 +0700

    init branch

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App txt="Manchester United 2 - 0 Hull City" cat={1} />, document.getElementById('app'))
+ReactDOM.render(<App val={0} />, document.getElementById('app'))
